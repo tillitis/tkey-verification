00005929c016a2b7158f34b94b13cbf2a2a12a0c
Michael Cardell Widerkrantz <mc@tillitis.se>
2024-06-24 18:25:07 +0200

    Add comments on our assets
    Explain the assets and their functions:
    
    - appbins
    - firmwares
    - vendor pubkeys

--- cmd/tkey-verification/firmwares.go
+++ cmd/tkey-verification/firmwares.go
@@ -22,10 +22,14 @@ type Firmware struct {
 	Size int
 }
 
+// Firmwares is a dictionary of all known firmwares, index by the
+// first part of the UDI, UDI0.
 type Firmwares struct {
 	firmwares map[hardware]Firmware
 }
 
+// GetFirmware returns what we know about a firmware indexed by a UDI,
+// or an error if no firmare is found.
 func (f Firmwares) GetFirmware(udi tkey.UDI) (*Firmware, error) {
 	hw, err := newHardware(udi.VendorID, udi.ProductID, udi.ProductRev)
 	if err != nil {
@@ -49,6 +53,11 @@ func (f Firmwares) List() []string {
 	return list
 }
 
+// NewFirmwares initialises all known firmwares. It returns a map of
+// all firmwares or an error.
+//
+// To add a new firmware to the database, use addFirmware() in this
+// function.
 func NewFirmwares() (Firmwares, error) {
 	fws := Firmwares{
 		firmwares: make(map[hardware]Firmware),
@@ -122,7 +131,7 @@ func (f *Firmwares) addFirmware(udi0BEhex string, vendorID uint16, productID uin
 	}
 
 	// Safety check. We compare the passed UDI0 argument to what
-	// we computed from vendori ID, product ID, and product
+	// we computed from vendor ID, product ID, and product
 	// revision. If it's not the same, we bail.
 	if udi0BEhex != hw.toUDI0BEhex() {
 		return EqualError{one: "udi0BEhex arg", two: "calculated"}
@@ -146,8 +155,11 @@ type hardware struct {
 	ProductRev uint8 // 6 bits
 }
 
+// newHardware is a utility function to generate a hardware struct
+// from a UDI we probably got from talking to a TKey.
 func newHardware(vendorID uint16, productID uint8, productRev uint8) (*hardware, error) {
 	const sixBitsMax = 2*2*2*2*2*2 - 1
+
 	if productID > sixBitsMax {
 		return nil, RangeError{what: "product ID"}
 	}
@@ -165,6 +177,7 @@ func newHardware(vendorID uint16, productID uint8, productRev uint8) (*hardware,
 func (h hardware) toUDI0BEhex() string {
 	var udi0BE [4]byte
 	var vidBE [2]byte
+
 	binary.BigEndian.PutUint16(vidBE[:], h.VendorID)
 	// 4 reserved bits | first 4 bits of 1st vendorID byte
 	udi0BE[0] = 0x0 | ((vidBE[0] & 0b11110000) >> 4)
@@ -174,5 +187,6 @@ func (h hardware) toUDI0BEhex() string {
 	udi0BE[2] = ((vidBE[1] & 0b00001111) << 4) | ((h.ProductID & 0b111100) >> 2)
 	// last 2 bits of productID | 6 bits productRev
 	udi0BE[3] = ((h.ProductID & 0b000011) << 6) | h.ProductRev
+
 	return hex.EncodeToString(udi0BE[:])
 }
