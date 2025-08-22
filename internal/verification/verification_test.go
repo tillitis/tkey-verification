0b568dae30cc36654466bff0fd2d4bfbd5267290
Michael Cardell Widerkrantz <mc@tillitis.se>
2025-08-22 18:41:10 +0200

    Add initial tests for verification
    - Parsing test
    - Proof verification with digest only

---
+++ internal/verification/verification_test.go
@@ -0,0 +1,124 @@
+package verification
+
+import (
+	"bytes"
+	"crypto/ed25519"
+	"crypto/sha512"
+	"fmt"
+	"testing"
+
+	"github.com/tillitis/tkey-verification/internal/tkey"
+	"sigsum.org/sigsum-go/pkg/crypto"
+	sumcrypto "sigsum.org/sigsum-go/pkg/crypto"
+	"sigsum.org/sigsum-go/pkg/key"
+	"sigsum.org/sigsum-go/pkg/policy"
+)
+
+const verificationProofJSON = `
+{
+  "timestamp": "2023-03-03T09:31:51Z",
+  "apptag":"verisigner-v0.0.3",
+  "apphash":"f8ecdcda53a296636a0297c250b27fb649860645626cc8ad935eabb4c43ea3e1841c40300544fade4189aa4143c1ca8fe82361e3d874b42b0e2404793a170142",
+  "proof": "version=1\nlog=4e89cc51651f0d95f3c6127c15e1a42e3ddf7046c5b17b752689c402e773bb4d\nleaf=1b6d f3744a4d05231ceed5d704e7fcdd8ee436f2253d980cf5716ae34cc16c06f439 55b94e42482923adf99d49bc5b3aa3b96093139817284ced6403167d47db0fa3091d8282adea6a1e11ebe3f1f42bf76ddceb8b3f0bbec4dd96f8f6908760bd0e\n\nsize=4186\nroot_hash=be8491d96283e89afb0014b82be85ba70cccb9e672fdbf595f701fb456e46cd0\nsignature=880584e76411f6ac7d2d7f85166b320cd08d6a775cf9b78969ff185dc2b6410c34e4ac2d3aa78d64285ab2a3259177b787da0d5326b71d034c6a7471a9dc5e0d\ncosignature=70b861a010f25030de6ff6a5267e0b951e70c04b20ba4a3ce41e7fba7b9b7dfc 1755522370 8f4507b9e9076ebab7f9234108de5e3b0bc1836a4933c0ac69ad338461537c8d6a0fcf93d2b7dc2416fbcd73c6b19dd3d2a311313920d7191a2ee7591cb73c04\ncosignature=b95ef35a9ffb3cf516f423a04128d37d3bffc74d4096bd5e967990c53d09678a 1755522370 4970b9b159c3e723ed11c5a75b70be027d5d4ba51e66209263ac779d52ce93ac9a1a3d798551274df0ffb05ea5e82e929fb3ece062fba676515f851d1f6bf005\ncosignature=0d4f46a219ab309cea48cde9712e7d8486fc99802f873175eeab70fb84b4f5a4 1755522370 65bc6e601f91d1faf81ec5a6aa7b24d11b89bcd1ea9e570c0a122c236bda7058500f2dc5706b16fa6853145922835034f561197e7d03156ddfe070d58aa7c40c\ncosignature=49c4cd6124b7c572f3354d854d50b2a4b057a750f786cf03103c09de339c4ea3 1755522370 b57f4ee89b3b0f39303e72fa7359afd696abaa759d9e255788355bb420607522c5b5cbde72ce6943b67e029858cf795609b2ff726dd11c61edfa37bfef323d07\ncosignature=42351ad474b29c04187fd0c8c7670656386f323f02e9a4ef0a0055ec061ecac8 1755522370 9c16d95c617a97f2da89b1d594510950725a0d537ddd5e76eac5d5ef87de9ea9b2d249467c2a4c078cff6edbf87ecc25b08b6b5cbfecb42efb5439408d75930f\ncosignature=1c997261f16e6e81d13f420900a2542a4b6a049c2d996324ee5d82a90ca3360c 1755522370 fe82dc7e0f051492be67b9ebce4fe6c3b488e3286e10f55363655c6391a132eb8c645ec7a4e414a81c6b2d8d149331bdb1e4bdd5609ff79009174d76f17c4302\n\nleaf_index=4185\nnode_hash=9364bfbdef749709839140a812c628abf7f6b9a324d748056c8661b283f4a427\nnode_hash=e6d3a8151cdff24f78931465cfe5f9083080b18d86831a0e8f581c16061af439\nnode_hash=d8cf3ff208c18ddc4f3b21acb70f9ac12c14989a6372a4bb8602596423bed265\nnode_hash=9a062feda65100c4b5cfbc4297e9a07af77565b53fd77719ab75b8d31107f247\nnode_hash=9ca6b461d616cf790a32a967574087298abb4cd0c3da938b7fed143b7d92b5ec\n"
+}
+`
+
+func TestParseVerification(t *testing.T) {
+	var v Verification
+
+	if err := v.FromJson([]byte(verificationProofJSON)); err != nil {
+		t.Fatal(err)
+	}
+}
+
+// func TestVerifySignature(t *testing.T) {
+// 	var v Verification
+// 	var udi tkey.UDI
+
+// 	pubKey := []byte()
+
+// 	fwDigest := []byte("06d0aafcc763307420380a8c5a324f3fccfbba6af7ff6fe0facad684ebd69dd43234c8531a096c77c2dc3543f8b8b629c94136ca7e257ca560da882e4dbbb025")
+
+// 	if err := v.FromJson([]byte(verJSON)); err != nil {
+// 		t.Fatal(err)
+// 	}
+
+// 	appBins, err := appbins.NewAppBins()
+// 	vendorKeys, err := vendorkey.NewVendorKeys(appBins)
+// 	if err != nil {
+// 		t.Logf("no vendor signing public key: %v", err)
+// 		t.FailNow()
+// 	}
+
+// 	// Verify vendor's signature over known message.
+// 	msg, err := buildMessage(udi.Bytes, fwDigest, pubKey)
+// 	if err != nil {
+// 		t.Fatal("couldn't build message")
+// 	}
+// 	_, err = v.VerifySig(msg, vendorKeys)
+// 	if err != nil {
+// 		t.Fatal("vendor signature verification failed")
+// 	}
+// }
+
+func TestVerifyProof(t *testing.T) {
+	var v Verification
+
+	submitKey := mustParsePublicKey("ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIONFrsjCVeDB3KwJVsfr/kphaZZZ9Sypuu42ahZBjeya sigsum key")
+
+	sigsumKeys := map[crypto.Hash]crypto.PublicKey{crypto.HashBytes(submitKey[:]): submitKey}
+
+	witnessKey := mustParsePublicKey("ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFw1KBko6do5a+7eXyKiJRpYnmrG3lKk3oXehjT/zK9t TKey")
+	logKey, err := sumcrypto.PublicKeyFromHex("4644af2abd40f4895a003bca350f9d5912ab301a49c77f13e5b6d905c20a5fe6")
+	if err != nil {
+		panic(err)
+	}
+
+	policy, err := policy.NewKofNPolicy([]sumcrypto.PublicKey{logKey}, []sumcrypto.PublicKey{witnessKey}, 1)
+	if err != nil {
+		panic(err)
+	}
+
+	digest, err := sumcrypto.HashFromHex("ae5155e5f4c4fdc81f976f5613750b3f5cba011391a55aa20c90b07c58b3cbaf")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if err := v.FromJson([]byte(verificationProofJSON)); err != nil {
+		t.Fatal(err)
+	}
+
+	if err := v.VerifyProofDigest(digest, *policy, sigsumKeys); err != nil {
+		t.Fatal("vendor signature not verified")
+	}
+}
+
+// TODO copy from cmd/tkey-verification/common.go - Move?
+func buildMessage(udiBE, fwHash, pubKey []byte) ([]byte, error) {
+	var buf bytes.Buffer
+
+	if l := len(udiBE); l != tkey.UDISize {
+		return nil, fmt.Errorf("wrong length of UDI")
+	}
+	buf.Write(udiBE)
+
+	if l := len(fwHash); l != sha512.Size {
+		return nil, fmt.Errorf("wrong digest size")
+	}
+	buf.Write(fwHash)
+
+	if l := len(pubKey); l != ed25519.PublicKeySize {
+		return nil, fmt.Errorf("wrong public key size")
+	}
+	buf.Write(pubKey)
+
+	return buf.Bytes(), nil
+}
+
+func mustParsePublicKey(ascii string) sumcrypto.PublicKey {
+	key, err := key.ParsePublicKey(ascii)
+	if err != nil {
+		panic(err)
+	}
+	return key
+}
